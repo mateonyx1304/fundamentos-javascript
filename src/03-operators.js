4d2f6dd97b73b80ee7ee5f94744c76c7760b37bb
Mateo Angel <mateonyx1304@gmail.com>
2026-04-27 18:49:17 -0500

    03-operators.js

---
+++ src/03-operators.js
@@ -0,0 +1,75 @@
+// Operadores Aritméticos
+
+// Suma (+)
+const suma = 5+3; // 8
+console.log (suma);
+
+
+// Resta (-)
+const resta = 5-3; // 2
+console.log (resta);
+
+
+// Multiplicación (*)
+const multiplicacion = 5*3; // 15
+console.log (multiplicacion);
+
+
+// División (/)
+const division = 6/3; // 2
+console.log (division);
+
+
+// Módulo (%)
+const modulo = 5 % 2; // El resultado es el residuo de la división, en este caso 1, porque 5 dividido por 2 da un cociente de 2 y un residuo de 1.
+console.log (modulo);
+
+// Exponenciación (**)
+const exponenciacion = 5**3; // 125
+console.log (exponenciacion);
+
+
+// Operadores de Asignación
+
+// Asignación (=)
+let x = 5; // Asigna el valor 5 a la variable x
+console.log (x);
+
+// Asignación de Suma (+=)
+x += 3; // Equivale a x = x + 3, ahora x es 8
+console.log (x);
+
+// Asignación de Resta (-=)
+x -= 2; // Equivale a x = x - 2, ahora x es 6
+console.log (x);
+
+// Asignación de Multiplicación (*=)
+x *= 4; // Equivale a x = x * 4, ahora x es 24
+console.log (x);
+
+// Asignación de División (/=)
+x /= 6; // Equivale a x = x / 6, ahora x es 4
+console.log (x);
+
+// Asignación de Módulo (%=)
+x %= 3; // Equivale a x = x % 3, ahora x es 1
+console.log (x);
+
+// Asignación de Exponenciación (**=)
+x **= 2; // Equivale a x = x ** 2, ahora x es 1
+console.log (x);
+
+
+
+// Operadores de incremento y decremento
+
+// Incremento (++)
+let y = 5;
+y++; // Equivale a y = y + 1, ahora y es 6
+console.log (y);
+
+// Decremento (--)
+y--; // Equivale a y = y - 1, ahora y es 5
+console.log (y);
+
+
