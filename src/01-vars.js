fb2bfeade26187248c53fbf9fd5633a574ae85c9
Mateo Angel <mateonyx1304@gmail.com>
2026-04-27 14:55:05 -0500

    retos 1 y 2

---
+++ src/01-vars.js
@@ -0,0 +1,21 @@
+var bloquesRojos = true;
+var bloquesRojos = 5;
+
+console.log(bloquesRojos);
+
+let contador = 0;
+let mensaje = "hola";
+contador = 5;
+
+console.log(contador, mensaje);
+
+const pi = 3.1416;
+console.log(pi);
+
+console.log(nombre);
+var nombre = "Juan";
+
+var nombre;
+console.log(nombre);
+nombre = "Pedro";
+console.log(nombre);
---
+++ src/02-types.js
@@ -0,0 +1,22 @@
+// Tipos de datos Primitivos en JavaScript
+
+const texto = "Hola, soy un texto"; // String
+const numero = 42; // Number
+const booleano = true; // Boolean
+const nulo = null; // Null
+const indefinido = undefined; // Undefined
+const simbolo = Symbol("ID"); // Symbol
+const grande = 123n; // BigInt
+
+console.log("Texto:", texto);
+console.log("Número:", numero);
+console.log("Booleano:", booleano);
+console.log("Nulo:", nulo);
+console.log("Indefinido:", indefinido);
+console.log("Símbolo:", simbolo);
+console.log("BigInt:", grande);
+
+// Tipos de datos no primitivos (Objetos) COMPLEJOS
+const objeto = { nombre: "Juan", edad: 30 }; // Object
+const arreglo = [1, 2, 3, "string"];
+const funcion = function () {};
