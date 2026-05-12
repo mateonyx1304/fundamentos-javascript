0fdb3d30eae6ec462e0bc459d20fd5d060ca1895
Mateo Angel <mateonyx1304@gmail.com>
2026-05-11 19:14:57 -0500

    objects, destructuracion

--- src/15-objects.js
+++ src/15-objects.js
@@ -17,3 +17,57 @@ console.log(nota[campo]);
 console.log(nota.author?.name);
 
 console.log(nota.author.name);
+
+
+// Destructuracion
+
+const nota2 = {
+    id: 1,
+    title: 'Mi primera nota',
+    content: 'Contenido de la nota',
+    createdAt: Date.now(),
+}
+
+const id = nota2.id;
+const title = nota2.title;
+
+const {title:titulo, content} = nota2;
+console.log(titulo, content)
+
+// Spread
+
+const nota3 = {id: 2, title: 'hola'};
+const data = {esAdmin: true};
+
+const copia = {... nota3};
+console.log(nota3);
+console.log(copia);
+
+const notaActualizada = {
+    ... nota3,
+    ...data,
+    edad: 20,
+}
+console.log(notaActualizada);
+
+// Verificar
+
+const nota4 = {
+    id: 1,
+    title: 'titulo de la nota',
+    content: 'contenido de la nota',
+    createdAt: Date.now(),
+};
+console.log('title' in nota4);
+
+
+// Object.keys
+
+console.log(Object.keys(nota4));
+
+// Object.values
+console.log(Object.values(nota4));
+
+// Object.entries
+
+console.log(Object.entries(nota4));
