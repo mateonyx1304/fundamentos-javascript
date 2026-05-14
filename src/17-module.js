467e477c0bc7a64264218a4b3de2e6d96ba8ed50
Mateo Angel <mateonyx1304@gmail.com>
2026-05-13 20:38:51 -0500

    Agrega soporte para ES Modules

---
+++ src/17-module.js
@@ -0,0 +1,8 @@
+// ESM
+
+import { pi, sumar, restar } from "./math.js";
+
+
+console.log(pi);
+console.log(sumar(2, 2));
+console.log(restar(10, 3));
---
+++ src/math.js
@@ -0,0 +1,9 @@
+export const pi = 3.1416;
+
+export function sumar(a, b){
+    return a + b;
+}
+
+export function restar(a, b) {
+    return a - b;
+}
